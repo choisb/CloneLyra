4e563c264a58b6f85f5555bbf56fefb1fb9a04b3
Dev_SBee <s004402@gmail.com>
2024-12-11 23:21:26 +0900

    프로젝트 초기 세팅

---
+++ Source/CloneLyra/CloneLyra.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CloneLyra.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CloneLyra, "CloneLyra" );
