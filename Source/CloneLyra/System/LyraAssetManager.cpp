1e47cee6ec1f23c6ab2441e7bca9ff55293bd108
Dev_SBee <s004402@gmail.com>
2025-02-04 22:18:53 +0900

    LyraAssetManager 추가

---
+++ Source/CloneLyra/System/LyraAssetManager.cpp
@@ -0,0 +1,71 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LyraAssetManager.h"
+#include "CloneLyra/LyraLogChannels.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(LyraAssetManager)
+
+ULyraAssetManager::ULyraAssetManager()
+	:Super()
+{
+}
+
+ULyraAssetManager& ULyraAssetManager::Get()
+{
+	check(GEngine);
+
+	if (ULyraAssetManager* Singleton = Cast<ULyraAssetManager>(GEngine->AssetManager))
+	{
+		return *Singleton;
+	}
+
+	UE_LOG(LogLyra, Fatal, TEXT("Invalid AssetManagerClassName in DefaultEngine.ini.  It must be set to LyraAssetManager!"));
+
+	// Fatal error above prevents this from being called.
+	return *NewObject<ULyraAssetManager>();
+}
+
+UObject* ULyraAssetManager::SynchronousLoadAsset(const FSoftObjectPath& AssetPath)
+{
+	if (AssetPath.IsValid())
+	{
+		TUniquePtr<FScopeLogTime> LogTimePtr;
+
+		if (ShouldLogAssetLoads())
+		{
+			LogTimePtr = MakeUnique<FScopeLogTime>(*FString::Printf(TEXT("Synchronously loaded asset [%s]"), *AssetPath.ToString()), nullptr, FScopeLogTime::ScopeLog_Seconds);
+		}
+
+		if (UAssetManager::IsInitialized())
+		{
+			return UAssetManager::GetStreamableManager().LoadSynchronous(AssetPath, false);
+		}
+
+		// Use LoadObject if asset manager isn't ready yet.
+		return AssetPath.TryLoad();
+	}
+
+	return nullptr;
+}
+
+bool ULyraAssetManager::ShouldLogAssetLoads()
+{
+	static bool bLogAssetLoads = FParse::Param(FCommandLine::Get(), TEXT("LogAssetLoads"));
+	return bLogAssetLoads;
+}
+
+void ULyraAssetManager::AddLoadedAsset(const UObject* Asset)
+{
+	if (ensureAlways(Asset))
+	{
+		FScopeLock LoadedAssetsLock(&LoadedAssetsCritical);
+		LoadedAssets.Add(Asset);
+	}
+}
+
+void ULyraAssetManager::StartInitialLoading()
+{
+	Super::StartInitialLoading();
+
+	UE_LOG(LogLyra, Display, TEXT("ULyraAssetManager::StartInitialLoading"));
+}
---
+++ Source/CloneLyra/System/LyraAssetManager.h
@@ -0,0 +1,104 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Engine/AssetManager.h"
+#include "LyraAssetManager.generated.h"
+
+/**
+ * ULyraAssetManager
+ *
+ *	Game implementation of the asset manager that overrides functionality and stores game-specific types.
+ *	It is expected that most games will want to override AssetManager as it provides a good place for game-specific loading logic.
+ *	This class is used by setting 'AssetManagerClassName' in DefaultEngine.ini.
+ */
+UCLASS()
+class CLONELYRA_API ULyraAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+
+public:
+	ULyraAssetManager();
+
+	// Returns the AssetManager singleton object.
+	static ULyraAssetManager& Get();
+
+	// Returns the asset referenced by a TSoftObjectPtr.  This will synchronously load the asset if it's not already loaded.
+	template<typename AssetType>
+	static AssetType* GetAsset(const TSoftObjectPtr<AssetType>& AssetPointer, bool bKeepInMemory = true);
+
+	// Returns the subclass referenced by a TSoftClassPtr.  This will synchronously load the asset if it's not already loaded.
+	template<typename AssetType>
+	static TSubclassOf<AssetType> GetSubclass(const TSoftClassPtr<AssetType>& AssetPointer, bool bKeepInMemory = true);
+
+protected:
+	static UObject* SynchronousLoadAsset(const FSoftObjectPath& AssetPath);
+	static bool ShouldLogAssetLoads();
+
+	// Thread safe way of adding a loaded asset to keep in memory.
+	void AddLoadedAsset(const UObject* Asset);
+
+	virtual void StartInitialLoading() override;
+
+private:
+	
+	// Assets loaded and tracked by the asset manager.
+	UPROPERTY()
+	TSet<TObjectPtr<const UObject>> LoadedAssets;
+
+	// Used for a scope lock when modifying the list of load assets.
+	FCriticalSection LoadedAssetsCritical;
+
+};
+
+template<typename AssetType>
+AssetType* ULyraAssetManager::GetAsset(const TSoftObjectPtr<AssetType>& AssetPointer, bool bKeepInMemory)
+{
+	AssetType* LoadedAsset = nullptr;
+
+	const FSoftObjectPath& AssetPath = AssetPointer.ToSoftObjectPath();
+
+	if (AssetPath.IsValid())
+	{
+		LoadedAsset = AssetPointer.Get();
+		if (!LoadedAsset)
+		{
+			LoadedAsset = Cast<AssetType>(SynchronousLoadAsset(AssetPath));
+			ensureAlwaysMsgf(LoadedAsset, TEXT("Failed to load asset [%s]"), *AssetPointer.ToString());
+		}
+
+		if (LoadedAsset && bKeepInMemory)
+		{
+			// Added to loaded asset list.
+			Get().AddLoadedAsset(Cast<UObject>(LoadedAsset));
+		}
+	}
+
+	return LoadedAsset;
+}
+
+template<typename AssetType>
+TSubclassOf<AssetType> ULyraAssetManager::GetSubclass(const TSoftClassPtr<AssetType>& AssetPointer, bool bKeepInMemory)
+{
+	TSubclassOf<AssetType> LoadedSubclass;
+
+	const FSoftObjectPath& AssetPath = AssetPointer.ToSoftObjectPath();
+
+	if (AssetPath.IsValid())
+	{
+		LoadedSubclass = AssetPointer.Get();
+		if (!LoadedSubclass)
+		{
+			LoadedSubclass = Cast<UClass>(SynchronousLoadAsset(AssetPath));
+			ensureAlwaysMsgf(LoadedSubclass, TEXT("Failed to load asset class [%s]"), *AssetPointer.ToString());
+		}
+
+		if (LoadedSubclass && bKeepInMemory)
+		{
+			// Added to loaded asset list.
+			Get().AddLoadedAsset(Cast<UObject>(LoadedSubclass));
+		}
+	}
+
+	return LoadedSubclass;
+}
